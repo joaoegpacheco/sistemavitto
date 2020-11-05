cedccfa0d4eeeb700422a49b7074897fe059c5a1
Joao Pacheco <jao.pacheco1@gmail.com>
2020-11-04 23:06:25 -0300

    Melhoria de código

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-let A = 200;
-let B = 100;
-let C = 50;
-let D = 20;
-let E = 10;
-let F = 5;
-let G = 2;
+let notas = [200, 100, 50, 20, 10, 5, 2];
 
-let notas = [A, B, C, D, E, F, G];
-
-let saldoInicial = function (valor, index) {
+const saldoInicial = (valor, index) => {
 
     let i = index;
 
@@ -18,7 +10,7 @@ let saldoInicial = function (valor, index) {
     return i;
 }
 
-let sacarRec = function (valor, index) {
+const sacarRecur = (valor, index = 0) => {
 
     let saldoMax = saldoInicial(valor, index);
 
@@ -36,7 +28,7 @@ let sacarRec = function (valor, index) {
 
         for (let j = i; j < notas.length; j++) {
 
-            let sacarResto = sacarRec(resto, j);
+            let sacarResto = sacarRecur(resto, j);
 
             if (sacarResto.length > 0) {
                 sacarResto.unshift(notas[i]);
@@ -47,20 +39,45 @@ let sacarRec = function (valor, index) {
     return [];
 }
 
-let sacar = function (valor) {
-    return sacarRec(valor, 0);
+const sacar = (valor) => {
+    return sacarRecur(valor);
 }
 
 let valorSacado = prompt('Quanto quer sacar?');
 
+const createH1Principal = () => {
+    let h1Principal = document.createElement('h1');
+    h1Principal.textContent = "Exercício - Sistema Vitto";
+    document.body.appendChild(h1Principal);
+}
+createH1Principal();
+
+if (isNaN(valorSacado) || valorSacado < 1) {
+    let valorDiferenteDeNumero = document.createElement('div');
+    valorDiferenteDeNumero.innerHTML = "Valor inválido! Favor digite um número inteiro entre 1 e 1000.";
+    document.body.appendChild(valorDiferenteDeNumero);
+}
+
 if (valorSacado > 1000) {
-    console.log("Não é possível sacar esse valor, pois o caixa permite saque até R$1.000,00");
+    let valorMaiorQueMil = document.createElement('div');
+    valorMaiorQueMil.innerHTML = "Não é possível sacar esse valor, pois o caixa permite saque até R$1.000,00";
+    document.body.appendChild(valorMaiorQueMil);
 }
 
 if (valorSacado == 1 || valorSacado == 3) {
-    console.log("Não é possível devolver este valor, pois não existe nota de R$1,00");
+    let valorDeUmOuTres = document.createElement('div');
+    valorDeUmOuTres.innerHTML = "Não é possível devolver este valor, pois não existe nota de R$1,00";
+    document.body.appendChild(valorDeUmOuTres);
 }
 
-if (valorSacado > 0 && valorSacado <= 1000) {
-    console.log("  " + sacar(valorSacado).toString());
+const createValorFinal = () => {
+    let valorFinal = document.createElement('div');
+    let stringValorFinal = document.createElement('p');
+    if (valorSacado > 0 && valorSacado <= 1000) {
+        stringValorFinal.innerHTML = "As notas recebidas foram: ";
+        valorFinal.innerHTML = "  " + sacar(valorSacado).toString();
+        document.body.appendChild(stringValorFinal);
+        document.body.appendChild(valorFinal);
+    }
 }
+createValorFinal();
